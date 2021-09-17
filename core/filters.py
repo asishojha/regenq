5662ed80961ddbb3ff64521b94c7a56c7d9649ba
singlagarvit <singlagarvit68@gmail.com>
2021-09-17 14:27:06 +0530

    Second Commit

--- core/filters.py
+++ core/filters.py
@@ -4,9 +4,8 @@ import django_filters
 class StudentFilter(django_filters.FilterSet):
 	name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 	f_name = django_filters.CharFilter(field_name='f_name', lookup_expr='icontains')
-	school__school = django_filters.CharFilter(field_name='school__school', lookup_expr='iexact')
 
 
 	class Meta:
 		model = Student
-		fields = ['name', 'f_name', 'school__school']
+		fields = ['name', 'f_name']
--- core/views.py
+++ core/views.py
@@ -4,18 +4,33 @@ from .models import Student
 from .filters import StudentFilter
 
 def index(request):
-	data = request.GET
-	name = data.get('name')
-	father_name = data.get('f_name')
-	reg_no = data.get('reg_no')
-	s_index = data.get('s_index')
+	data = request.GET.copy()
+	name = data.get('name', '')
+	father_name = data.get('f_name', '')
+	reg_no = data.get('reg_no', '')
+	s_index = data.get('school__school', '')
 
 	if reg_no:
-		students = Student.objects.filter(regno=reg_no)
+		students = Student.objects.filter(regno__iexact=reg_no)
+
 	else:
-		students = Student.objects.all()
-		student_filter = StudentFilter(request.GET, queryset=students)
-		students = student_filter.qs
+		if s_index != '':
+			students = Student.objects.filter(school__school__iexact=s_index)
+			if name != '' and father_name != '':
+				students = students.filter(Q(name__icontains=name) & Q(f_name__icontains=father_name))
+			elif name != '' and father_name == '':
+				students = students.filter(name__icontains=name)
+			elif name == '' and father_name != '':
+				students = students.filter(f_name__icontains=father_name)
+		else:
+			if name != '' and father_name != '':
+				students = Student.objects.filter(Q(name__icontains=name) & Q(f_name__icontains=father_name))
+			elif name != '' and father_name == '':
+				students = Student.objects.filter(name__icontains=name)
+			elif name == '' and father_name != '':
+				students = Student.objects.filter(f_name__icontains=father_name)
+			else:
+				students = Student.objects.none()
 
 	context = {
 		'students': students
